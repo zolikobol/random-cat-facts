b55a093d1d47186b9d274b2445c228b3aa7c577d
LAPTOP-S2ELGDMB\zoli <zoltan.kobol@globallogic.com>
2017-11-21 18:48:29 +0100

    second test

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,77 @@
 const functions = require('firebase-functions')
-const request = require('request')
+const https = require('https')
 const { DialogflowApp } = require('actions-on-google')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-
-  let app = new DialogflowApp({
-    request: request,
-    response: response
-  })
-
-  let actions = app.getIntent();
-
-  const Actions = {
-    WELCOME: 'input.welcome',
-    RANDOM_FACT: 'random.fact'
-  }
-
-  function welcomeIntent() {
-    app.ask('Hello, I`m your cat fact Agent. I can tell you random facts about cats. How can I help you?')
-  }
-
-  function rendomCatFact() {
-    request.get('https://catfact.ninja/fact', (error, response, body) => {
-      let result = JSON.parse(body);
-      app.tell(`Here is a random fact: ${result.fact}`);
+exports.randomCatFacts = functions.https.onRequest((request, response) => {
+    const app = new DialogflowApp({
+        request: request,
+        response: response
     })
-  }
 
+    let action = app.getIntent();
+
+    console.log(action)
+
+    const Actions = {
+        WELCOME: 'input.welcome',
+        RANDOM_CAT_FACT: 'random.fact',
+        BASIC_CARD_INTENT: 'basic.card.intent'
+    }
+
+    switch (action) {
+        case Actions.WELCOME:
+            welcomeIntent(app)
+            break;
+        case Actions.RANDOM_CAT_FACT:
+        randomCatFactIntent(app)
+            break;
+        case Actions.BASIC_CARD_INTENT:
+            basicCardIntent(app)
+            break;
+    
+        default:
+            break;
+    }
+
+    function welcomeIntent(app) {
+        app.ask('Hello! I`m your Funny Random Cat Fact Assistant. Ask me for a random cat fact.')
+    }
+
+    function randomCatFactIntent(app) {
+        let data = ''
+        https.get({
+            hostname: 'catfact.ninja',
+            path: '/fact'
+        }, (res) => {
+            res.on('data', (chunk) => {
+                data += chunk
+            })
+            res.on('error', (err) => {
+                console.log(err)
+            })
+            res.on('end', () => {
+                console.log(data)
+                let result = JSON.parse(data)
+                app.ask(`Here is a random cat fact: ${result.fact}`)
+            })
+        })
+    }
+
+    function basicCardIntent(app) {
+        app.askWithCarousel('Which of these looks good?',
+        app.buildCarousel()
+         .addItems([
+           app.buildOptionItem('SELECTION_ONE',
+             ['synonym of KEY_ONE 1', 'synonym of KEY_ONE 2'])
+             .setTitle('Number one')
+             .setDescription('Description one'),
+           app.buildOptionItem('SELECTION_TWO',
+             ['synonym of KEY_TWO 1', 'synonym of KEY_TWO 2'])
+             .setTitle('Number two')
+             .setDescription('Description 2'),
+         ]))
+    }
 });
