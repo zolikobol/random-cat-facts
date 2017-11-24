e10f6398ebb7d01904cb72df211942966a594de9
Kobol Zoltan <zoltan.kobol@globallogic.com>
2017-11-24 08:56:23 +0100

    changes

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,53 @@
 const functions = require('firebase-functions')
-const request = require('request')
+const https = require('https');
 const { DialogflowApp } = require('actions-on-google')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.catFacts = functions.https.onRequest((request, response) => {
 
+  console.log('beginning')
   let app = new DialogflowApp({
     request: request,
     response: response
   })
 
-  let actions = app.getIntent();
+  console.log('after dialogflow')
 
+  let action = app.getIntent()
+
+  console.log('intent ' + action)
   const Actions = {
     WELCOME: 'input.welcome',
     RANDOM_FACT: 'random.fact'
   }
 
-  function welcomeIntent() {
-    app.ask('Hello, I`m your cat fact Agent. I can tell you random facts about cats. How can I help you?')
+  if(action == Actions.WELCOME) {
+    welcomeIntent(app)
+  } else if(action == Actions.RANDOM_FACT) {
+    rendomCatFact(app)
   }
 
-  function rendomCatFact() {
-    request.get('https://catfact.ninja/fact', (error, response, body) => {
-      let result = JSON.parse(body);
-      app.tell(`Here is a random fact: ${result.fact}`);
+  function welcomeIntent(agent) {
+    console.log('welcome')
+    agent.ask('Hello, I`m your cat fact Agent. I can tell you random facts about cats. How can I help you?')
+  }
+
+  function rendomCatFact(agent) {
+    console.log('random fact')
+    let data = '';
+    https.get({
+      hostname: 'catfact.ninja',
+      path: '/fact'
+    }, (res) => {
+      res.on('data', (chunk) => {
+        data += chunk
+      })
+      res.on('end', () => {
+        let result = JSON.parse(data)
+        agent.tell(result.fact)
+      })
+      res.on('error', (err) => {
+        console.log(err)
+      })
     })
   }
-
 });
